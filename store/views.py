6c40b53b7a287448001d7eb1edf036d7ccf20b5c
nursultanit <nursultanzamirovic@gmail.com>
2024-08-14 23:38:12 +0600

    fsdfdsf

---
+++ store/views.py
@@ -0,0 +1,99 @@
+from rest_framework import viewsets, generics, status, permissions
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.urls import *
+from .serializers import (
+    UserSerializer, LoginSerializer, UserProfileSerializer,
+    CategorySerializer, ProductSerializer, ProductPhotosSerializer,
+    RatingSerializer, ReviewSerializer
+)
+from .models import UserProfile, Category, Product, ProductPhotos, Rating, Review
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
+from .models import Product
+from .serializers import ProductSerializer
+
+
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = RefreshToken.for_user(user)
+        return Response({
+            'user': {
+                'email': user.email,
+                'username': user.username,
+                'token': str(token.access_token),
+            }
+        }, status=status.HTTP_201_CREATED)
+
+
+class CustomLoginView(TokenObtainPairView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except Exception:
+            return Response({"detail": "Неверные учетные данные"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        user = serializer.validated_data['user']
+        refresh = RefreshToken.for_user(user)
+        response = HttpResponseRedirect(reverse('product_list'))
+        response.set_cookie('token', str(refresh.access_token))
+        return response
+
+
+class LogoutView(generics.GenericAPIView):
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.data.get("refresh")
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    filter_backends = [DjangoFilterBackend]
+
+
+    def get_queryset(self):
+        return super().get_queryset()
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['category', 'price']
+
+class ProductPhotosViewSet(viewsets.ModelViewSet):
+    queryset = ProductPhotos.objects.all()
+    serializer_class = ProductPhotosSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
